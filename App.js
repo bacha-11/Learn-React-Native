3c237849c1304699c6f3b112292cae2b2d869142
bacha-11 <ijazbacha14@gmail.com>
2021-10-05 14:37:45 +0500

    create a react native project and then create a simple button and onPress change the value of Text

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [outputText, setoutPutText] = useState('Open up App.js to start working on your app!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={()=>setoutPutText("Text his been changed!")} />
       <StatusBar style="auto" />
     </View>
   );

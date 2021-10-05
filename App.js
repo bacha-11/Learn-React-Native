2024f6b14044a9775eca8ce79024cc85889c5972
bacha-11 <ijazbacha14@gmail.com>
2021-10-05 18:41:33 +0500

    handle inputText data in react native

--- App.js
+++ App.js
@@ -3,13 +3,23 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [goal, setGoal] = useState('')
+
+  const goalInputHandler = (goal) =>{
+    setGoal(goal)
+  }
+
+
+  const addGoalHandler = () =>{
+    console.log(goal)
+  }
   
   return (
     <View style={styles.screen}>
 
       <View style={styles.inputBtnStyle}>
-        <TextInput placeholder="Course Goal" style={styles.inputStyle} />
-        <Button title="+" style={{with:"10%"}} />
+        <TextInput placeholder="Course Goal" value={goal} style={styles.inputStyle} onChangeText={goalInputHandler} />
+        <Button title="+" style={{with:"10%"}} onPress={addGoalHandler} />
       </View>
 
       <View>

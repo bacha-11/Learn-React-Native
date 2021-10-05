4dbddc1cfd0f68306d502f4757153fdb45aba786
bacha-11 <ijazbacha14@gmail.com>
2021-10-05 17:30:22 +0500

    use inline style with react native

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   
   return (
-    <View>
+    <View style={{padding:30}}>
 
-      <View>
-        
+      <View style={{flexDirection:"row", justifyContent:"space-evenly", alignItems:"center"}}>
+        <TextInput placeholder="Course Goal" style={{marginTop:30,  width:"80%", marginBottom:10, borderBottomColor:"black", borderBottomWidth:1}} />
+        <Button title="+" style={{with:"10%"}} />
       </View>
 
       <View>
-        
+      
       </View>
       
     </View>
